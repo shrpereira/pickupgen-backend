d4d8a87ed2f8e7d8d8ce41bae3d18ae85d326dd3
silvioper@gmail.com <100senha>
2017-11-05 21:21:23 -0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/pickupLine", (req, res) => {
+    const pickupLines = require("./pickupline.json");
+    const randomNumber = Math.floor(Math.random() * pickupLines.length);
+
+    res.status(200).json(pickupLines[randomNumber]);
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
